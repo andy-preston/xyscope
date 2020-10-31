6bdd8f8ac1bd859ab1f940a52a080c5a27daa542
Andy Preston <edgeeffect@gmail.com>
2020-10-31 14:30:59 +0000

    Use webpack for building

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+    'entry': './src/vue-xyscope.js',
+    'mode': 'development',
+    'output': {
+        'filename': 'vue-xyscope.js',
+        'path': path.resolve(__dirname, 'build'),
+    },
+    'module': {
+        'rules': [
+            {
+                'test': /\.js$/,
+                'exclude': /node_modules/,
+                'use': {
+                    'loader': 'babel-loader'
+                }
+            }
+        ]
+    }
+}
