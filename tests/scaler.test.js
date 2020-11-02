fcb9b35c656445a54f7e3a1aa15da0bed4962799
Andy Preston <edgeeffect@gmail.com>
2020-11-02 14:38:48 +0000

    tidied up scaler tests

--- tests/scaler.test.js
+++ tests/scaler.test.js
@@ -1,31 +1,31 @@
 /* eslint-env jest */
 
-const scaler = require('../src/scaler');
+const Scaler = require('../src/scaler');
 
 test('scales correctly with positive coordinates', () => {
-    const testScaler = scaler();
-    testScaler.setLimits(0, 0, 20, 10);
-    expect(testScaler.scale(5, 'x', 20)).toBe(5);
-    expect(testScaler.scale(5, 'x', 40)).toBe(10);
-    expect(testScaler.scale(5, 'y', 10)).toBe(5);
-    expect(testScaler.scale(5, 'y', 40)).toBe(20);
+    const scaler = Scaler();
+    scaler.setLimits(0, 0, 20, 10);
+    expect(scaler.scale(5, 'x', 20)).toBe(5);
+    expect(scaler.scale(5, 'x', 40)).toBe(10);
+    expect(scaler.scale(5, 'y', 10)).toBe(5);
+    expect(scaler.scale(5, 'y', 40)).toBe(20);
 });
 
 test('scales correctly with negative coordinates', () => {
-    const testScaler = scaler();
-    testScaler.setLimits(-20, -10, 0, 0);
-    expect(testScaler.scale(-15, 'x', 20)).toBe(5);
-    expect(testScaler.scale(-15, 'x', 40)).toBe(10);
-    expect(testScaler.scale(-5, 'y', 10)).toBe(5);
-    expect(testScaler.scale(-5, 'y', 40)).toBe(20);
+    const scaler = Scaler();
+    scaler.setLimits(-20, -10, 0, 0);
+    expect(scaler.scale(-15, 'x', 20)).toBe(5);
+    expect(scaler.scale(-15, 'x', 40)).toBe(10);
+    expect(scaler.scale(-5, 'y', 10)).toBe(5);
+    expect(scaler.scale(-5, 'y', 40)).toBe(20);
 });
 
 test('scales correctly with mixed coordinates', () => {
-    const testScaler = scaler();
-    testScaler.setLimits(-10, -5, 10, 5);
-    expect(testScaler.scale(5, 'x', 20)).toBe(15);
-    expect(testScaler.scale(5, 'x', 40)).toBe(30);
-    expect(Math.abs(testScaler.scale(5, 'y', 10))).toBe(0);
-    expect(testScaler.scale(0, 'y', 40)).toBe(20);
+    const scaler = Scaler();
+    scaler.setLimits(-10, -5, 10, 5);
+    expect(scaler.scale(5, 'x', 20)).toBe(15);
+    expect(scaler.scale(5, 'x', 40)).toBe(30);
+    expect(Math.abs(scaler.scale(5, 'y', 10))).toBe(0);
+    expect(scaler.scale(0, 'y', 40)).toBe(20);
 });
 
