92654a67abff023f4d7fd9c676b481343f404b23
Andy Preston <edgeeffect@gmail.com>
2020-11-02 14:38:37 +0000

    tidied up buffer tests

--- tests/buffer.test.js
+++ tests/buffer.test.js
@@ -1,14 +1,14 @@
 /* eslint-env jest */
 
-const buffer = require('../src/buffer');
+const Buffer = require('../src/buffer');
 
 test('buffer is correct size and contains zeroed elements', () => {
     [10, 20, 30].forEach((size) => {
-        const testBuffer = buffer(size);
-        expect(testBuffer.size()).toBe(size);
+        const buffer = Buffer(size);
+        expect(buffer.size()).toBe(size);
         // If an element is "undefined" forEach will not "see" it.
         for (let idx = 0; idx < size; idx++) {
-            let element = testBuffer.element(idx);
+            let element = buffer.element(idx);
             expect(element.x).toBe(0);
             expect(element.y).toBe(0);
         }
@@ -16,20 +16,16 @@ test('buffer is correct size and contains zeroed elements', () => {
 });
 
 test("can't request an item beyond end of buffer", () => {
-    const testBuffer = buffer(5);
-    expect(() => {
-        testBuffer.element(6);
-    }).toThrow();
-    expect(() => {
-        testBuffer.element(-1);
-    }).toThrow();
+    const buffer = Buffer(5);
+    expect(() => { buffer.element(6); }).toThrow();
+    expect(() => { buffer.element(-1); }).toThrow();
 });
 
-test('pushed element is always first in buffer, followed by others in push order', () => {
-    const testBuffer = buffer(5);
+test('pushed element is always first, followed by others in push order', () => {
+    const buffer = Buffer(5);
     for (let item = 1; item <= 10; item++) {
-        testBuffer.push({ 'x': item, 'y': item });
-        testBuffer.forEach((idx, actual) => {
+        buffer.push({ 'x': item, 'y': item });
+        buffer.forEach((idx, actual) => {
             let expected = item - idx;
             if (expected < 0) {
                 expected = 0;
@@ -41,9 +37,9 @@ test('pushed element is always first in buffer, followed by others in push order
 });
 
 test('pushed element comes out in the order it went in', () => {
-    const testBuffer = buffer(5);
-    testBuffer.push({ 'x': 4, 'y': 5 });
-    const actual = testBuffer.element(0);
+    const buffer = Buffer(5);
+    buffer.push({ 'x': 4, 'y': 5 });
+    const actual = buffer.element(0);
     expect(actual.x).toBe(4);
     expect(actual.y).toBe(5);
 });
