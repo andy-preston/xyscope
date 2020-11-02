9241ec171e69e8135fb50a6181dd1d844ccd1757
Andy Preston <edgeeffect@gmail.com>
2020-11-02 18:20:46 +0000

    add some JSDoc to component

--- src/vue-xyscope.js
+++ src/vue-xyscope.js
@@ -8,6 +8,9 @@
             '<canvas id="canvas" style="width: 100%; height: 100%"></canvas>' +
             '</div>',
         'props': {
+            /**
+             * @member {string} limits "xMin, yMin, xMax, yMax"
+             */
             'limits': {
                 'type': String,
                 'required': true,
@@ -30,6 +33,16 @@
             }
         },
         'methods': {
+            /**
+             * Push data is a method rather than a watcher on a property because
+             * two consecutive data items could well be the same as each other
+             * at which point the watcher won't be triggered.
+             * When the parent receives a 'request-data' event it should respond
+             * by passing data to this method.
+             *
+             * @function pushData
+             * @param {object} data the x,y pair (``{'x': 23, 'y': 42}``)
+             */
             'pushData': (data) => {
                 scope.pushData(data);
             }
