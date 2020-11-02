ce581d926de9e173c60719e138501db4754f0bb4
Andy Preston <edgeeffect@gmail.com>
2020-11-02 18:14:50 +0000

    wrap component definition inside a function to provide a closure for private scope

--- src/vue-xyscope.js
+++ src/vue-xyscope.js
@@ -1,41 +1,44 @@
 /* global Vue */
 
-Vue.component('xyscope', {
-    'template': '<div class="xyscope">' +
-        '<canvas id="canvas" style="width: 100%; height: 100%"></canvas>' +
-        '</div>',
-    'props': {
-        'limits': {
-            'type': String,
-            'required': true,
-            'validator': (value) => {
-                return this.scope.validateLimits(value);
+(() => {
+    Vue.component('xyscope', {
+        'template': '<div class="xyscope">' +
+            '<canvas id="canvas" style="width: 100%; height: 100%"></canvas>' +
+            '</div>',
+        'props': {
+            'limits': {
+                'type': String,
+                'required': true,
+                'validator': (value) => {
+                    return this.scope.validateLimits(value);
+                }
             }
-        }
-    },
-    'data': () => {
-        return {
-        }
-    },
-    'watch': {
-        'limits': {
-            'immediate': true,
-            // eslint-disable-next-line no-unused-vars
-            'handler': (val, oldVal) => {
-                this.scope.setLimits(val);
+        },
+        'data': () => {
+            return {
             }
+        },
+        'watch': {
+            'limits': {
+                'immediate': true,
+                // eslint-disable-next-line no-unused-vars
+                'handler': (val, oldVal) => {
+                    this.scope.setLimits(val);
+                }
+            }
+        },
+        'methods': {
+            'push': (x, y) => {
+                this.scope.push(x, y);
+            }
+        },
+        'beforeCreate': () => {
+            this.scope = require('../src/scope')();
+        },
+        'mounted': () => {
+            this.scope.bindCanvas(document.getElementById('canvas'));
         }
-    },
-    'methods': {
-        'push': (x, y) => {
-            this.scope.push(x, y);
-        }
-    },
-    'beforeCreate': () => {
-        this.scope = require('../src/scope')();
-    },
-    'mounted': () => {
-        this.scope.bindCanvas(document.getElementById('canvas'));
-    }
 
-});
+    });
+
+})();
