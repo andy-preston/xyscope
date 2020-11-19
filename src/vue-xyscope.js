9c61322a9217e53de964b927e9d825b56cf7c4a9
Andy Preston <edgeeffect@gmail.com>
2020-11-19 16:29:37 +0000

    Rebuilding component in line with style guide

--- src/vue-xyscope.js
+++ src/vue-xyscope.js
@@ -1,13 +1,10 @@
 import Vue from 'vue';
-import Scope from './scope.js';
+import { Scope } from './scope.js';
 
 const scope = Scope();
 
+// eslint-disable-next-line vue/component-definition-name-casing
 Vue.component('xyscope', {
-    'template': '<div class="xyscope">' +
-        '<canvas id="canvas" style="width: 100%; height: 100%"' +
-        " v-on:request-data=\"$emit('request-data')\"> " +
-        '</canvas></div>',
     'props': {
         /**
          * @member {string} limits "xMin, yMin, xMax, yMax"
@@ -27,6 +24,9 @@ Vue.component('xyscope', {
             }
         }
     },
+    'mounted': () => {
+        scope.start(document.getElementById('canvas'));
+    },
     'methods': {
         /**
          * Push data is a method rather than a watcher on a property because
@@ -40,8 +40,8 @@ Vue.component('xyscope', {
          */
         'pushData': scope.pushData
     },
-    'mounted': () => {
-        scope.start(document.getElementById('canvas'));
-    }
-
+    'template': '<div class="xyscope">' +
+        '<canvas id="canvas" style="width: 100%; height: 100%"' +
+        " v-on:request-data=\"$emit('request-data')\"> " +
+        '</canvas></div>'
 });
